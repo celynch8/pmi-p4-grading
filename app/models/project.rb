0f137dbba72f83b62df52102568b198c3fd6896f
Colleen Lynch <celynch8@gmail.com>
2015-02-28 22:01:33 -0500

    updated project show & update views

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,22 +1,11 @@
 class Project < ActiveRecord::Base
+#Student-related methods
 	def get_student_1
 		Student.find(self.student_1_id)
 	end
 
 	def get_student_2
 		Student.find(self.student_2_id) if self.student_2_id
-	end
-
-	def set_student_ids(s1_id,s2_id)
-		s1 = Student.find(s1_id)
-		s1.update(project_id: self.id)
-		self.update(student_1_id: s1_id)
-
-		if s2_id
-			s2 = Student.find(s2_id)
-			s2.update(project_id: self.id)
-			self.update(student_2_id: s2_id)
-		end
 	end
 
 	def get_students
@@ -31,6 +20,19 @@ class Project < ActiveRecord::Base
 		str
 	end
 
+	def set_student_ids(s1_id,s2_id)
+		s1 = Student.find(s1_id)
+		s1.update(project_id: self.id)
+		self.update(student_1_id: s1_id)
+
+		if s2_id
+			s2 = Student.find(s2_id)
+			s2.update(project_id: self.id)
+			self.update(student_2_id: s2_id)
+		end
+	end
+
+#Test-related methods
 	def graded?
 		hash = self.attributes
 		for key in hash.keys do
@@ -40,4 +42,19 @@ class Project < ActiveRecord::Base
 		end
 		return true
 	end
+
+	# def objective?
+	# 	for objective in pro
+	# 		if @project.objective1
+	# 			return @project.objective1
+	# 			<ul>
+	# 				<li><%= @project.objective1_goal1 %> </li>
+	# 				<li><%= @project.objective1_goal2 %> </li>
+	# 				<li><%= @project.objective1_goal3 %> </li>
+	# 				<li><i>Stretch Goal: </i><%= @project.objective1_goal4 %> </li>
+	# 			</ul>
+	# 		end
+
+
+
 end
